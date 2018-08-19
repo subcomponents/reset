b48a73e7726c32680d442b02e0eeb1d11653f6d5
Allan Moreno <hello@heyallan.com>
2018-08-18 22:41:24 -0600

    Redesign variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,56 @@
 var gulp         = require('gulp');
 var less         = require('gulp-less');
-var sourcemaps   = require('gulp-sourcemaps');
 var autoprefix   = require('gulp-autoprefixer');
 var minify       = require('gulp-minify-css');
 var rename       = require('gulp-rename');
 var header       = require('gulp-header');
 var nunjucks     = require('gulp-nunjucks-render');
 var pkgJson      = require('./package.json');
-var argv         = require('yargs').argv;
-var baseURL      = (argv.production) ? pkgJson.demo.url : '';
-var banner       = ['/** <%= package.name %> | <%= package.demo.url %> */\n'];
+var browserSync  = require('browser-sync').create();
+var banner       = ['/** (C) License <%= package.license %> | <%= package.repo.url %> */\n\n'];
 
 gulp.task('css', function() {
-  return gulp.src('src/less/bundle.less')
+  return gulp.src('./src/less/bundle.less')
     .pipe(less())
     .pipe(autoprefix())
     .pipe(rename(pkgJson.keyword + '.css'))
     .pipe(header(banner, { package: pkgJson }))
-    .pipe(gulp.dest('dist')) // <-- deliver expanded for dist
+    .pipe(gulp.dest('./dist')) // <-- deliver expanded for dist
     .pipe(minify())
     .pipe(rename(pkgJson.keyword + '.min.css'))
     .pipe(header(banner, { package: pkgJson }))
-    .pipe(gulp.dest('dist')) // <-- deliver compressed for dist
-    .pipe(gulp.dest('docs')) // <-- deliver extra copy for docs
+    .pipe(gulp.dest('./dist')) // <-- deliver compressed for dist
+    .pipe(gulp.dest('./docs')) // <-- deliver extra copy for docs
+    .pipe(browserSync.stream())
 })
 
 gulp.task('html', function() {
-  return gulp.src('src/docs/pages/**/*.njk')
+  return gulp.src('./src/docs/pages/**/*.njk')
     .pipe(nunjucks({
-      path: 'src/docs/partials',
-      data: { package: pkgJson, baseURL: baseURL }
+      path: './src/docs/partials',
+      data: { package: pkgJson }
     }))
-    .pipe(gulp.dest('docs'));
+    .pipe(gulp.dest('./docs'))
+    .pipe(browserSync.stream())
 })
 
-gulp.task('default', ['css', 'html'], function() {
-  gulp.watch('src/less/*', ['css', 'html']),
-  gulp.watch('src/docs/**/*', ['html'])
+gulp.task('readme', function() {
+  return gulp.src('./src/readme/*.njk')
+    .pipe(nunjucks({
+      ext: '.md',
+      data: { package: pkgJson }
+    }))
+    .pipe(gulp.dest('./'))
 })
+
+gulp.task('server', function() {
+  browserSync.init({ server: "./docs", open: false })
+})
+
+gulp.task('watch', function() {
+  gulp.watch('./src/less/**/*', ['css']);
+  gulp.watch('./src/docs/**/*', ['html']);
+  gulp.watch('./src/readme/**/*', ['readme']);
+})
+
+gulp.task('default', ['css', 'html', 'readme', 'server', 'watch'])
--- notes.md
+++ notes.md
@@ -2,3 +2,5 @@
 
 ***
 ---
+
+- switch to postcss
